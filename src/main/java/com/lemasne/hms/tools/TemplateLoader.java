f8003c49232824fd57345230c4fc887a6df992e2
magikorion <christophe.db@gmail.com>
2014-05-01 00:00:52 +0200

    Adding missing committed files

---
+++ src/main/java/com/lemasne/hms/tools/TemplateLoader.java
@@ -0,0 +1,32 @@
+package com.lemasne.hms.tools;
+
+import java.awt.Toolkit;
+import java.awt.Window;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class TemplateLoader {
+
+    public static void load(String templateName) {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if (templateName.equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+            try {
+                UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                JOptionPane.showMessageDialog(null, "Cannot load " + templateName + " template.");
+            }
+        }
+    }
+
+    public static void initWindowProperties(Window frame) {
+        // frame icon
+        frame.setIconImage(Toolkit.getDefaultToolkit().getImage(TemplateLoader.class.getResource("/pictures/hospital.png")));
+    }
+}
